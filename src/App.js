86e98cfe9853587bda442f51e6dc20858b43a88b
Arnav <arnavbiswas2000@gmail.com>
2021-01-01 16:47:22 +0530

    version 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const count = 400;
+    const section = document.querySelector(".App");
+    let i = 0;
+
+    while (i < count) {
+      const x = Math.floor(Math.random() * window.innerWidth);
+      const y = Math.floor(Math.random() * window.innerHeight);
+
+      const size = Math.random() * 4;
+
+      const star = document.createElement("i");
+
+      star.style.left = x + "px";
+      star.style.top = y + "px";
+      star.style.width = 1 + size + "px";
+      star.style.height = 1 + size + "px";
+
+      const blink_duration = Math.random() * 2;
+
+      star.style.animationDelay = 2 + blink_duration + "s";
+
+      section.appendChild(star);
+      i++;
+    }
+
+    window.addEventListener("resize", function () {
+      window.location.reload();
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="circle_wrap"></div>
+      <div className="circle_wrap_2"></div>
+      <div className="circle"></div>
+      <div className="yr_wp">
+        <div className="hny">Happy New Year</div>
+        <div className="yr">2021</div>
+      </div>
+      <div className="my_name">-- by Arnav</div>
     </div>
   );
 }
